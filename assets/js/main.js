1a5939d5366dbf2baac9c46b849c1ced91ea93dc
Ivo Zinenda <izinenda@gmail.com>
2019-07-09 08:59:50 +0200

    first deal

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.sidenav').sidenav();
+	$('.modal').modal();
+	$('.slider').slider({
+		height: 600
+	});
+	$('.carousel').carousel();
+	$('.tabs').tabs();
+	$('.scrollspy').scrollSpy();
+	$('.fixed-action-btn').floatingActionButton();
+});
