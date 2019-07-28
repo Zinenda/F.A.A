66d4bda9a5e2c69574ad0c98ef322097bc2e9dd6
Ivo Zinenda <izinenda@gmail.com>
2019-07-28 09:22:16 +0200

    some layout and plugins fixs

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,26 +8,50 @@ $(document).ready(function(){
 	$('.scrollspy').scrollSpy();
 	$('.fixed-action-btn').floatingActionButton();
 
-	$('.owl-carousel').owlCarousel({
+	var owl = $('.owl-carousel');
+	owl.owlCarousel({
 		loop:true,
+		nav:true,
 		margin:10,
-		responsiveClass:true,
 		responsive:{
 			0:{
-				items:1,
-				nav:true
+				items:1
 			},
 			600:{
-				items:2,
-				nav:true
+				items:2
+			},            
+			960:{
+				items:3
 			},
-			1000:{
-				items:3,
-				nav:true,
-				loop:false
+			1200:{
+				items:4
 			}
 		}
 	});
+	owl.on('mousewheel', '.owl-stage', function (e) {
+		if (e.deltaY>0) {
+			owl.trigger('next.owl');
+		} else {
+			owl.trigger('prev.owl');
+		}
+		e.preventDefault();
+	});
+
+	var owl = $('.owl-carousel .parceiros-carousel');
+	owl.owlCarousel({
+		items:6,
+		loop:true,
+		margin:2,
+		autoplay:true,
+		autoplayTimeout:2000,
+		autoplayHoverPause:true
+	});
+	$('.play').on('click',function(){
+		owl.trigger('play.owl.autoplay',[1000])
+	})
+	$('.stop').on('click',function(){
+		owl.trigger('stop.owl.autoplay')
+	})
 
 	var $container = $('#masonry-grid');
     // initialize
